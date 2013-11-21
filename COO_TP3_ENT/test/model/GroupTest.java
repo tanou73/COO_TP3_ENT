454b8a0b572b4fa4ad53431256b586656e03b477
hugo <hugo.chiavenuto@gmail.com>
2013-11-21 17:38:06 +0100

    Commit:   - More tests !!!

--- COO_TP3_ENT/test/model/GroupTest.java
+++ COO_TP3_ENT/test/model/GroupTest.java
@@ -53,9 +53,56 @@ public class GroupTest {
         
         entCtrl.createGroup(groupName);
         
-        Group grp = entCtrl.getUserGroup(groupName);
-        
+        Group grp = entCtrl.getUserGroup(groupName);        
         assertTrue(model.getGroups().contains(grp));
     }
     
+    @Test(expected = UnauthorisedException.class)
+    public void createGroupUnhautorised() throws UnauthorisedException, DuplicateItemException, BadArgumentException {
+        String groupName = "test";
+        
+        entCtrl.createGroup(groupName);
+    }
+
+    @Test(expected = DuplicateItemException.class)
+    public void createGroupDuplicate() throws UnauthorisedException, DuplicateItemException, BadArgumentException {
+        User bob = new User("Bob");
+        User morane = new User("Morane");
+        String groupName = "test";
+
+        entCtrl.connectUser(bob);
+        entCtrl.createGroup(groupName);
+        
+        entCtrl.connectUser(morane);
+        entCtrl.createGroup(groupName);
+    }
+    
+    @Test(expected = BadArgumentException.class)
+    public void createGroupBadArgument() throws UnauthorisedException, DuplicateItemException, BadArgumentException {
+        User bob = new User("Bob");
+        entCtrl.connectUser(bob);
+        entCtrl.createGroup(null);
+    }
+    
+    @Test
+    public void joinGroup() throws UnauthorisedException, DuplicateItemException, BadArgumentException {
+        User bob = new User("Bob");
+        User morane = new User("Morane");
+        String groupName = "test";
+
+        entCtrl.connectUser(bob);
+        
+        entCtrl.createGroup(groupName);
+        
+        entCtrl.connectUser(morane);
+        
+        entCtrl.joinGroup(groupName);
+        
+        Group grp = entCtrl.getUserGroup(groupName);   
+        assertTrue(grp.getUsers().contains(bob));
+        assertTrue(grp.getUsers().contains(morane));
+        
+        assertTrue(bob.getUserGroups().contains(grp));
+        assertTrue(morane.getUserGroups().contains(grp));
+    }
 }
